dca9f314c925e1463a3fd28c8a24d9df6fe3867c
andrey <kopylov.a@ext-system.com>
2022-06-26 14:49:05 +0700

    last update

--- HackPointFastFrontend/src/app/core/guards/auth.guard.ts
+++ HackPointFastFrontend/src/app/core/guards/auth.guard.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
+import {CookieService} from "ngx-cookie-service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private cookieService: CookieService) {
   }
 
   canActivate(
@@ -15,6 +16,7 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot)
     : Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
+    console.log(this.cookieService.check('_auth'));
     console.log(document.cookie.match(/^(.*;)?\s*_auth\s*=\s*[^;]+(.*)?$/));
     console.log(document.cookie);
     return true;
